6826932b5ffb9961a8ab6658b4232814b7569334
fabio-polystream <fabio@polystream.com>
2022-06-30 10:55:26 +0200

    Make graphics options react to streaming client GPU

--- Assets/Scripts/GameOptions/SpaceshipOptions.cs
+++ Assets/Scripts/GameOptions/SpaceshipOptions.cs
@@ -1,5 +1,7 @@
 using GameOptionsUtility;
 using GameplayIngredients;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
@@ -36,7 +38,6 @@ public class SpaceshipOptions : GameOption
         set => PlayerPrefs.SetInt(Preferences.keyboardScheme, (int)value);
     }
 
-
     public UpsamplingMethod upsamplingMethod
     {
         get => (UpsamplingMethod)PlayerPrefs.GetInt(Preferences.upsamplingMethod, (int)UpsamplingMethod.EASU_FSR);
@@ -66,11 +67,24 @@ public class SpaceshipOptions : GameOption
         if(!init)
         {
             DynamicResolutionHandler.SetDynamicResScaler(SetDynamicResolutionScale, DynamicResScalePolicyType.ReturnsPercentage);
+
+            var gpuScore = GetGPU3DMark();
+            UpdateRenderTargetResolution(gpuScore);
+            UpdateQualityLevel(gpuScore);
+
             init = true;
         }
 
         UpdateUpscalingMethod();
         UpdateFPSControlScheme();
+
+        Debug.LogFormat("Applying Graphics Options");
+
+        var graphicsOption = GameOption.Get<GraphicOption>();
+        if (graphicsOption != null)
+        {
+            graphicsOption.Apply();
+        }
     }
 
     public FPSKeys fpsKeys { get; private set; }
@@ -142,14 +156,137 @@ public class SpaceshipOptions : GameOption
                 default:
                     throw new System.NotImplementedException("Should not happen");
             }
+        }
+    }
 
+    internal class AutoQuality
+    {
+        internal int level;
+        internal int score;
+    }
+
+    void UpdateQualityLevel(int gpuScore)
+    {
+        var graphicsOption = GameOption.Get<GraphicOption>();
+        if (graphicsOption == null)
+        {
+            return;
+        }
+
+        List<AutoQuality> autoQuality = new List<AutoQuality>();
+
+        // It is important these are added in ascending score order
+        autoQuality.Add(new AutoQuality { level = 0, score = 2000 });
+        autoQuality.Add(new AutoQuality { level = 1, score = 10000 });
+        autoQuality.Add(new AutoQuality { level = 2, score = 14000 });
+
+        // Auto select the best known resolution for a given GPU score, if it exists.
+        if (gpuScore > 0)
+        {
+            int scoredIndex = 0;
+            int selQuality = 0;
+            foreach (var quality in autoQuality)
+            {
+                selQuality = autoQuality[scoredIndex].level;
+                if (quality.score < gpuScore)
+                    scoredIndex++;
+                else break;
+            }
             
+            Debug.LogFormat("Auto selected quality level: {1} ({0})", QualitySettings.names[selQuality], selQuality);
 
+            graphicsOption.quality = selQuality;
+        }
+    }
+    internal class AutoResolution
+    {
+        internal int width;
+        internal int height;
+        internal int score;
+
+        internal int GetPixles()
+        {
+            return width * height;
+        }
+    }
+
+    void UpdateRenderTargetResolution(int gpuScore)
+    {
+        var graphicsOption = GameOption.Get<GraphicOption>();
+        if (graphicsOption == null)
+        {
+            return;
+        }
+
+        List<AutoResolution> autoRes = new List<AutoResolution>();
+
+        // It is important these are added in ascending score order
+        autoRes.Add(new AutoResolution { width = 1280, height = 720, score = 2000 });
+        autoRes.Add(new AutoResolution { width = 1600, height = 900, score = 8000 });
+        autoRes.Add(new AutoResolution { width = 1920, height = 1080, score = 14000 });
+        autoRes.Add(new AutoResolution { width = 2560, height = 1440, score = 18000 });
+        autoRes.Add(new AutoResolution { width = 3840, height = 2160, score = 24000 });
+
+        // Auto select the best known resolution for a given GPU score, if it exists.
+        if (gpuScore > 0)
+        {
+            int scoredIndex = 0;
+            Resolution selRes = new Resolution();
+            foreach (var scoredRed in autoRes)
+            {
+                selRes.width = autoRes[scoredIndex].width;
+                selRes.height = autoRes[scoredIndex].height;
+                if (scoredRed.score < gpuScore)
+                {
+                    scoredIndex++;
+                }
+                else break;
+            }
+
+            Debug.LogFormat("Auto selected resolution: {0}x{1}", selRes.width, selRes.height);
+
+            // Pick the closest available
+            var selPixels = selRes.width * selRes.height;
+            foreach (var res in Screen.resolutions.OrderBy(o => o.width))
+            {
+                var resPixels = res.width * res.height;
+                if (resPixels <= selPixels)
+                {
+                    graphicsOption.width = res.width;
+                    graphicsOption.height = res.height;
+                }
+            }
+
+            Debug.LogFormat("Best matched resolution: {0}x{1}", graphicsOption.width, graphicsOption.height);
         }
     }
 
     float SetDynamicResolutionScale()
     {
         return screenPercentage;
+    }
+
+    private int m_GPUScore = -1;
+    public int GetGPU3DMark()
+    {
+        string gpuName = SystemInfo.graphicsDeviceName;
+        if (m_GPUScore < 0)
+        {
+            if (gpuName.Contains("3DMARK-"))
+            {
+                int dashIndex = gpuName.IndexOf('-') + 1;
+                int closeRoundIndex = gpuName.IndexOf(')', dashIndex);
+                string scoreString = gpuName.Substring(dashIndex, closeRoundIndex - dashIndex);
+                m_GPUScore = int.Parse(scoreString);
+            }
+            else
+            {
+                m_GPUScore = 0;
+            }
+        }
+
+        Debug.LogFormat("GPU: {0} - {1}", gpuName, m_GPUScore);
+
+        return m_GPUScore;
     }
 }
